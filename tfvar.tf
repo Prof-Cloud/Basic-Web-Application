529cebb1bcd6c44b59d0f7d80475243933b0541a
Prof-Cloud <the.fire.dragon.mac@gmail.com>
2025-11-21 13:38:08 -0500

    Create tfvar.tf

---
+++ tfvar.tf
@@ -0,0 +1,9 @@
+data "aws_ami" "the_ami" {
+  most_recent = true
+  owners = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+}
